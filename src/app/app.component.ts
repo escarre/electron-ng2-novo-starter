fcee7d41d75683ba6f7f403d463b79e00486acc2
Elizabeth Carretto <elizabeth.carretto@bullhorn.com>
2017-08-25 10:37:57 -0500

    Add notification, camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+declare var Notification: any;
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'happy dog day!';
+  localstream: any;
+  cameraRolling = false;
+
+
+  notify() {
+      new Notification('Doggo', {
+          body: 'Notification time!'
+      });
+  }
+
+  openCamera() {
+    let n = navigator;
+    n.getUserMedia = ( n.getUserMedia || n['webkitGetUserMedia'] || n['mozGetUserMedia']  || n['msGetUserMedia'] );
+    if (!this.cameraRolling) {
+      this.cameraRolling = true;
+      navigator.getUserMedia({ video: true, audio: false },
+        (stream) => {
+          document.getElementById('camera')['src'] = URL.createObjectURL(stream);
+          this.localstream = stream;
+        }, () => {
+          alert('could not connect stream');
+        }
+      );
+    } else {
+      this.cameraOff();
+    }
+  }
+
+  private cameraOff() {
+      this.cameraRolling = false;
+      document.getElementById('camera')['src'] = '';
+      this.localstream.getTracks()[0].stop();
+  }
 }
